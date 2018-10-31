707c22053a45b4937e66e42872bc894fa2edaea2
Samuel Teixeira Santos <samuel.teixeira@embrapa.br>
2018-10-31 16:41:37 -0300

    added file to define some properties of app, organizing core code of gui

---
+++ gui/app_def.py
@@ -0,0 +1,12 @@
+"""Definitions for this application
+"""
+
+
+def get_app_definitions(app_prop):
+
+    return {
+        'app_root_title': ' py3100G810 - Collect of weight and humidity of grains',
+        'app_title_01': '   py3100G810 - Collect of weight  ',
+        'app_title_02': '           and humidity of grains',
+        'app_label_edit': 'ID code:',
+    }[app_prop]
--- gui/app_win.py
+++ gui/app_win.py
@@ -1,32 +1,40 @@
 """GUI class definition for main window
 """
 
-from tkinter import CENTER, X, YES
+from tkinter import CENTER, TOP, W, X, YES
 
-from tkinter.ttk import Frame, Label
+from tkinter.ttk import Entry, Frame, Label
+
+from gui.app_def import get_app_definitions
 
 
 class AppWin(Frame):
 
     def __init__(self, master=None, **kw):
 
+        self.bar_qr_code = None
+
         # root Tk
         self.master = master
 
-        self.master.title(kw.get(
-            'app_title',
-            ' py3100G810 - Collect of weight and humidity of grains'
-            )
+        self.master.title(
+            get_app_definitions('app_root_title')
         )
 
         Frame.__init__(self, master, **kw)
+
+        self.add_title_app()
+
+        self.add_ipt_id_code()
+
+    def add_title_app(self):
 
         Label(
             self,
             font='size 16 bold',
             text='{0}\n{1}'.format(
-                '   py3100G810 - Collect of weight  ',
-                '           and humidity of grains'
+                get_app_definitions('app_title_01'),
+                get_app_definitions('app_title_02')
             ),
             background='light blue',
             foreground='dark green'
@@ -35,3 +43,26 @@ class AppWin(Frame):
             expand=YES,
             anchor=CENTER
         )
+
+    def add_ipt_id_code(self):
+
+        Label(
+            self,
+            font='size 13 bold',
+            text=get_app_definitions('app_label_edit'),
+            background='white',
+            foreground='green'
+        ).pack(
+            anchor=W,
+            side=TOP,
+            fill=X
+        )
+
+        self.bar_qr_code = Entry(
+            self,
+            font='Courier 14 bold'
+        )
+
+        self.bar_qr_code.pack(fill=X)
+
+        self.bar_qr_code.focus()
