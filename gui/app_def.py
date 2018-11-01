de2ea38870cda136fe49186f7610f822b93facd8
Samuel Teixeira Santos <samuel.teixeira@embrapa.br>
2018-11-01 10:46:03 -0300

    include weight and humidity entry widgets

--- gui/app_def.py
+++ gui/app_def.py
@@ -1,12 +1,23 @@
 """Definitions for this application
+
+    Titles, labels, edits
+        colors foreground/background
+    Default text labels etc
 """
 
 
 def get_app_definitions(app_prop):
 
     return {
-        'app_root_title': ' py3100G810 - Collect of weight and humidity of grains',
-        'app_title_01': '   py3100G810 - Collect of weight  ',
-        'app_title_02': '           and humidity of grains',
+        'app_root_title': ' py3100G810 - Grains weight and humidity collector',
+        'app_title_01': '   py3100G810 - Grains weight and  ',
+        'app_title_02': '              humidity collector',
+        'app_title_bkgr': 'light blue',
+        'app_title_frgr': 'dark green',
         'app_label_edit': 'ID code:',
+        'app_lbledt_bkgr': 'white',
+        'app_lbledt_frgr': 'green',
+        'labelframe_weight_caption': 'Weight:',
+        'labelframe_humidity_caption': 'Humidity:'
+
     }[app_prop]
--- gui/app_win.py
+++ gui/app_win.py
@@ -1,9 +1,9 @@
 """GUI class definition for main window
 """
 
-from tkinter import CENTER, TOP, W, X, YES
+from tkinter import CENTER, DoubleVar, DISABLED, HORIZONTAL, TOP, W, X, YES
 
-from tkinter.ttk import Entry, Frame, Label
+from tkinter.ttk import Entry, Frame, Label, LabelFrame, Panedwindow
 
 from gui.app_def import get_app_definitions
 
@@ -12,7 +12,39 @@ class AppWin(Frame):
 
     def __init__(self, master=None, **kw):
 
+        # Entry widget to collect
+        # ID code used to identify
+        # weight and humidity  of grain
+
         self.bar_qr_code = None
+
+        # Pannel Window that contains
+        # LabelFrames widgets for weight and humidity
+        self.pw_wei_hum = None
+
+        # LabelFrame widget that contains
+        # Entry widget for weidght
+        self.lblfrm_weight = None
+
+        # Entry Widget to collect grain weight
+        # from G810 machine
+        self.ety_weight = None
+
+        # DoubleVar variable to hold the weight value
+        # between the Entry widget and to send to a database
+        self.weight_portion = DoubleVar(master, value=0.0)
+
+        # LabelFrame widget that contains
+        # Entry widget for humidity
+        self.lblfrm_hum = None
+
+        # Entry Widget to collect grain humidity
+        # from 3100 machine family
+        self.ety_humidity = None
+
+        # DoubleVar variable to hold the humidity value
+        # between the Entry widget and to send to a database
+        self.humidty_portion = DoubleVar(master, value=0.0)
 
         # root Tk
         self.master = master
@@ -27,7 +59,13 @@ class AppWin(Frame):
 
         self.add_ipt_id_code()
 
+        self.add_weight_humidity_panel()
+
     def add_title_app(self):
+        """define the Title application Label
+
+        :return: None
+        """
 
         Label(
             self,
@@ -36,8 +74,8 @@ class AppWin(Frame):
                 get_app_definitions('app_title_01'),
                 get_app_definitions('app_title_02')
             ),
-            background='light blue',
-            foreground='dark green'
+            background=get_app_definitions('app_title_bkgr'),
+            foreground=get_app_definitions('app_title_frgr')
         ).pack(
             fill=X,
             expand=YES,
@@ -45,13 +83,17 @@ class AppWin(Frame):
         )
 
     def add_ipt_id_code(self):
+        """define the id code input widget
+
+        :return:None
+        """
 
         Label(
             self,
             font='size 13 bold',
             text=get_app_definitions('app_label_edit'),
-            background='white',
-            foreground='green'
+            background=get_app_definitions('app_lbledt_bkgr'),
+            foreground=get_app_definitions('app_lbledt_frgr')
         ).pack(
             anchor=W,
             side=TOP,
@@ -66,3 +108,51 @@ class AppWin(Frame):
         self.bar_qr_code.pack(fill=X)
 
         self.bar_qr_code.focus()
+
+    def add_weight_humidity_panel(self):
+        """define the main Pannel Widget
+
+        :return: None
+        """
+
+        self.pw_wei_hum = Panedwindow(self, orient=HORIZONTAL)
+
+        self.pw_wei_hum.pack()
+
+        self.lblfrm_weight = LabelFrame(
+            self.pw_wei_hum,
+            text=get_app_definitions('labelframe_weight_caption')
+        )
+
+        self.pw_wei_hum.add(self.lblfrm_weight)
+
+        self.ety_weight = Entry(
+            self.lblfrm_weight,
+            font='Courier 32 bold',
+            justify=CENTER,
+            width=6,
+            textvariable=self.weight_portion
+        )
+
+        self.ety_weight.pack(padx=5, pady=5)
+
+        self.ety_weight.config(state=DISABLED)
+
+        self.lblfrm_hum = LabelFrame(
+            self.pw_wei_hum,
+            text=get_app_definitions('labelframe_humidity_caption')
+        )
+
+        self.pw_wei_hum.add(self.lblfrm_hum)
+
+        self.ety_humidity = Entry(
+            self.lblfrm_hum,
+            font='Courier 32 bold',
+            justify=CENTER,
+            width=6,
+            textvariable=self.humidty_portion
+        )
+
+        self.ety_humidity.pack(padx=5, pady=5)
+
+        self.ety_humidity.config(state=DISABLED)
